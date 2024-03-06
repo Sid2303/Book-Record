31d0a54e7cd2f9326015c6021054415b94347219
Sid2303 <subhashr898@gmail.com>
2024-03-07 00:14:18 +0530

    Day 1

--- index.js
+++ index.js
@@ -1,17 +1,32 @@
 const express = require("express");
 
+// Importing Routes
+const usersRouter = require("./routes/users");
+const booksRouter = require("./routes/books");
+
 const app = express();
 
 const PORT = 3000;
 
 app.use(express.json());
 
-app.get("/",(req,res)=>{
+app.get("/", (req, res)=>{
     res.status(200).json({
-        message: "Server is running",
-    })
+        message: "Server is up and running sucessfully",
+    });
+});
+
+app.use("/users", usersRouter);
+app.use("/books", booksRouter)
+
+
+
+app.get("*",(req, res)=>{
+    res.status(404).json({
+        message: "This route doesn't exist",
+    });
 })
 
-app.listen(PORT,()=>{
-    console.log("Server is running");
+app.listen(PORT, ()=>{
+    console.log(`Server is up and running at PORT ${PORT}`);
 })
